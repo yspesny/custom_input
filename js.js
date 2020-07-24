da9977e23c8ad17eeb12a62382a49ce4a4d6e8d6
Bogdan <lebed4086@gmail.com>
2020-07-24 21:06:46 +0300

    update

---
+++ js.js
@@ -0,0 +1,6 @@
+// $(document).ready(function () {
+//     $('.wrapper_checkbox').click(function (e) { 
+//         $('.wrapper_checkbox input').toggle('active');
+//         console.log('active');
+//     });
+// });
